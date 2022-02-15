a79300bd77067398017565b3e69cbd24fe70daaa
jpsrm <97506106+jpsrm@users.noreply.github.com>
2022-02-15 16:58:08 +0530

    Update cft1.tf

--- cft1.tf
+++ cft1.tf
@@ -3,18 +3,18 @@ provider "aws" {
 region = "ap-southeast-1"
 alias =  "Singapore"
 }
-variable "acc_no" {
+variable "acc_no1" {
   type = string
 }
 
-variable "access_key" {
+variable "access_key1" {
   type = string
 }
 
-variable "secret_access_key" {
+variable "secret_access_key1" {
   type = string
 }
-resource "aws_cloudformation_stack" "project" {
+resource "aws_cloudformation_stack" "project1" {
   name = "project-stack"
   capabilities     = ["CAPABILITY_NAMED_IAM"]
   template_url = "https://s3-projectnew1.s3.ap-south-1.amazonaws.com/project3.json"
